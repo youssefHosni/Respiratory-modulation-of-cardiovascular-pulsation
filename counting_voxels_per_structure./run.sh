0059e42e827185c2aaab3c94e4a0b52bef5bb730
youssefHosni <72076328+youssefHosni@users.noreply.github.com>
2020-11-06 19:38:19 +0200

    Create run.sh

---
+++ counting_voxels_per_structure./run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# the input is the "main_path" which has the file you want to count the voxels in and "input_file" which is the file which you want to count the number of signicant voxels in
+
+main_path= 
+input_file=
+
+bash main.sh -m $main_path -i $input_file 
